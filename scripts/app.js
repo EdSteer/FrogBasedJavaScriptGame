068a75d41d7d6012f020df9c75d678b30cf75033
Ed Steer <edsteer@yahoo.co.uk>
2021-10-14 13:50:11 +0100

    1410 13:50

--- scripts/app.js
+++ scripts/app.js
@@ -127,16 +127,9 @@ function init() {
 
   const lilypads = [1, 4, 7]
 
-//   const road = [
-//     {
-//       cssClass: 'road',
-//       position: 
-//     },
-//     {
-//       cssClass: 'river',
-//       position: 
-//     }
-// ]
+  const rivers = [0, 2, 3, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
+
+  const roads = [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89]
   
   
   
@@ -173,6 +166,8 @@ function init() {
     startMovement()
     addFrog(startingFrogPosition)
     scorePoints()
+    addRiver()
+    addRoad()
     
   }
   
@@ -184,6 +179,14 @@ function init() {
   function addLily() {
     lilypads.forEach(lily => cells[lily].classList.add('lilypad'))
     }
+
+  function addRiver() {
+    rivers.forEach(river => cells[river].classList.add('river')) 
+    }
+
+  function addRoad() {
+    roads.forEach(road => cells[road].classList.add('road'))
+  }
     
   
   function addFrog(position) {
